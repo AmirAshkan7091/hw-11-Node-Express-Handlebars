f48fd473604ada39dbb8b551d39ac149faddab3c
AmirAshkan7091 <amirashsd@yahoo.com>
2020-12-02 00:41:01 -0800

    make it better

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,11 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
+    host: "de1tmi3t63foh7fa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: "password",
-    database: "burger_db"
+    user: "ewmuvszyzuwc5pf6	",
+    password: "l2icr3hbzz8amqkg",
+    database: "xzgcmdimi8oxh9jf"
   });
 }
 
