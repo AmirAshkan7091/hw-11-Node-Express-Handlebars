fe9ab1df229e92b95ba42601c643055c3abda2ab
AmirAshkan7091 <amirashsd@yahoo.com>
2020-12-05 21:14:51 -0800

    amir

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,40 @@
-require("dotenv").config();
-const mysql = require("mysql");
-var connection;
-
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burger_db"
-  });
-}
-
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
+const mysql = require('mysql');
+// Creating a class for database controls
+const connectionType = () => {
+  if (process.env.JAWSDB_URL) {
+    return mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    return mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    });
   }
-  console.log("connected as id " + connection.threadId);
-});
-
-module.exports = connection;
+}
+class Database {
+  constructor() {
+    this.connection = connectionType();
+  }
+  //Made query() to a promise
+  query(sql, args) {
+    return new Promise((resolve, reject) => {
+      this.connection.query(sql, args, (err, rows) => {
+        if (err)
+          return reject(err);
+        resolve(rows);
+      });
+    });
+  }
+  //Made end() to a promise
+  close() {
+    return new Promise((resolve, reject) => {
+      this.connection.end(err => {
+        if (err)
+          return reject(err);
+        resolve();
+      });
+    });
+  }
+}
+module.exports = Database;
